917c8dc8126c67233346fd64ddd007dc7ef57af8
Samuel Valentine <samv8479@gmail.com>
2022-08-09 11:02:47 -0400

    add shrek teleport button

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-function moveShrek(){
-    
-   
- }
+function moveShrek() {
+    console.log("button was cliked")
+    shrek = document.getElementById("yourmom")
+    document.getElementById("yourmom")
+    shrek.style.left = `${Math.floor(Math.random()*(window.innerWidth.width))}-shrek.clientWidth}px`
+}

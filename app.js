59d93f5eee2907f676da0b2d7fda1e9aa555396e
Samuel Valentine <samv8479@gmail.com>
2022-08-12 11:25:45 -0400

    update site

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 function moveShrek() {
     console.log("button was cliked")
     shrek = document.getElementById("yourmom")
-    document.getElementById("yourmom")
-    shrek.style.left = `${Math.floor(Math.random()*(window.innerWidth.width))}-shrek.clientWidth}px`
+    shrek.style.left = `${Math.floor(Math.random()*(window.innerWidth))-shrek.clientWidth}px`
 }
